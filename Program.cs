5ee1b37886408beb41a83c178572d2c4942c37dc
betof <betof@DESKTOP-5PPJA4A>
2022-09-26 13:41:59 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+    
+        /*EJERCICIO 01*/
+        int numeroEntero=0;
+
+        //Mensaje en pantalla//
+        Console.WriteLine("EJERCICIO 01");
+        Console.WriteLine("Numero entero");
+
+    //siguiente instruccion recibe y almacena el número//
+        numeroEntero = Convert.ToInt32(Console.ReadLine());
+    if (numeroEntero >= 1)
+{
+    Console.WriteLine("El número es positivo");
+}
+    else if (numeroEntero <= -1)
+    {
+    Console.WriteLine("El número es negativo");
+    }
+    else if (numeroEntero==0)
+{
+    Console.WriteLine("El número es 0");
+}
